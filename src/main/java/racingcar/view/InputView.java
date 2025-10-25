2ac886d727ecff83bde4508e9d5194896810573f
harameter <haram1000068@gmail.com>
2025-10-26 03:52:30 +0900

    feat(view.InputView.java): 시도 횟수 문자열 입력 기능 추가

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -10,8 +10,7 @@ package racingcar.view;
 import camp.nextstep.edu.missionutils.Console;
 
 /**
- * InputView 클래스는 각종 입력(자동차 이름, 시도 횟수)을 받고
- * 이를 String, Integer 등 올바른 형태로 반환한다.
+ * InputView 클래스는 각종 입력(자동차 이름, 시도 횟수)을 받아 String 으로 반환한다.
  *
  * @version 0.1 2025/10/26
  * @author  정하람
@@ -20,6 +19,7 @@ public final class InputView {
 
 	private static final String NAMES_PROMPT =
 			"경주할 자동차 이름을 입력하세요.(이름은 쉼표(,)로 구분)";
+	private static final String TRY_COUNT_PROMPT = "시도할 횟수";
 
 	private InputView() {}
 
@@ -33,6 +33,20 @@ public final class InputView {
 		String line = Console.readLine();
 		if (line == null) {
 			throw new IllegalArgumentException("입력이 비어있습니다");
+		}
+		return line;
+	}
+
+	/**
+	 * readTryCountLine()은 자동차 이름을 입력받고 이를 String 으로 반환한다.
+	 * @return 들어온 입력을 String 으로 반환
+	 * @exception IllegalArgumentException 들어온 입력이 비어있을 경우 예외 발생 후 프로그램 종료
+	 */
+	public static String readTryCountLine() {
+		System.out.println(TRY_COUNT_PROMPT);
+		String line = Console.readLine();
+		if (line == null) {
+			throw new IllegalArgumentException("입력이 비어있습니다.");
 		}
 		return line;
 	}
