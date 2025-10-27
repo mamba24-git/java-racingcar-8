b6bc8061430081393c3c66480efce305e196ea6b
harameter <haram1000068@gmail.com>
2025-10-27 14:58:51 +0900

    feat(Application.java): 실제 프로그램 실행부 작성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
+/*
+ * Application.java
+ *
+ * ver 0.1
+ *
+ * 2025/10/25
+ */
 package racingcar;
 
+import racingcar.controller.RaceController;
+/**
+ * Application.java 실행 시퀀스
+ * GameController 실행
+ *
+ * @version 0.1 2025/10/25
+ * @author  정하람
+ */
 public class Application {
+    private Application() {}
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+            RaceController controller = new RaceController();
+            controller.run();
     }
 }
