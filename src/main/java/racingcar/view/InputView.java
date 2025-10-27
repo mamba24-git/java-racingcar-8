9a17ba5df835cbe5d55446a14fad02e75ed3a305
harameter <haram1000068@gmail.com>
2025-10-27 18:21:04 +0900

    fix(view.InputView.java): 입력값이 비어 있을 때도 예외가 제대로 발생하도록 수정

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -12,7 +12,7 @@ import camp.nextstep.edu.missionutils.Console;
 /**
  * InputView 클래스는 각종 입력(자동차 이름, 시도 횟수)을 받아 String 으로 반환한다.
  *
- * @version 0.1 2025/10/26
+ * @version 0.1 2025/10/27
  * @author  정하람
  */
 public final class InputView {
@@ -34,9 +34,11 @@ public final class InputView {
 		if (line == null) {
 			throw new IllegalArgumentException("입력이 비어있습니다");
 		}
+		if (line.trim().isEmpty()) {
+			throw new IllegalArgumentException("입력이 비어있습니다");
+		}
 		return line;
 	}
-
 	/**
 	 * readTryCountLine()은 자동차 이름을 입력받고 이를 String 으로 반환한다.
 	 * @return 들어온 입력을 String 으로 반환
@@ -46,7 +48,10 @@ public final class InputView {
 		System.out.println(TRY_COUNT_PROMPT);
 		String line = Console.readLine();
 		if (line == null) {
-			throw new IllegalArgumentException("입력이 비어있습니다.");
+			throw new IllegalArgumentException("입력이 비어있습니다");
+		}
+		if (line.trim().isEmpty()) {
+			throw new IllegalArgumentException("입력이 비어있습니다");
 		}
 		return line;
 	}
